ce03d8583a88a6a3141f08618a65c8476479a93c
JeaneMirele <jeanemirele7@gmail.com>
2025-03-30 22:14:28 -0300

    Test q2

--- __tests__/questao2.test.js
+++ __tests__/questao2.test.js
@@ -1,4 +1,4 @@
-import DuasPilhas from "../questao2";
+import DuasPilhas from "../src/Lista 01/questao2";
 let pilha;
 
 beforeEach(() => {
@@ -6,12 +6,12 @@ beforeEach(() => {
 })
 
 test ("Os elementos devem ser inseridos na pilha A e B corretamente", () => {
-   pilha.push1(1);
-   pilha.push1(2);
-   pilha.push2(8);
-   pilha.push2(9);
-    expect(pilha.array[9]).toBe(9);
-    expect(pilha.array[8]).toBe(8);
+   pilha.empilhaA(1);
+   pilha.empilhaA(2);
+   pilha.empilhaB(8);
+   pilha.empilhaB(9);
+    expect(pilha.array[9]).toBe(8);
+    expect(pilha.array[8]).toBe(9);
     expect(pilha.array[0]).toBe(1);
     expect(pilha.array[1]).toBe(2);
 
