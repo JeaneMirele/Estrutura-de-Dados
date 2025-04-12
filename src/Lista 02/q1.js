c6d9ea87ab16da59061e757801cea09eb30a3d3f
JeaneMirele <jeanemirele7@gmail.com>
2025-04-12 00:53:26 -0300

    q1

---
+++ src/Lista 02/q1.js
@@ -0,0 +1,49 @@
+import Fila from "./Fila";
+
+class PilhaComFila{
+    constructor(size = 5){
+        this.size = size;
+        this.fila = new Fila();
+        this.fila2 = new Fila();
+    }
+
+    push(dado){
+        if(this.isFull()) throw new Error ("Stack Overflow");
+        this.fila.enqueue(dado);
+    }
+
+    pop(){
+        if(this.isEmpty()) throw new Error ("Stack Underflow");
+       while(this.fila.length() > 1){
+            this.fila2.enqueue(this.fila.dequeue());
+       }
+
+    let ultimoE = this.fila.dequeue();
+       const filaAux = this.fila;
+       this.fila = this.fila2;
+       this.fila2 = filaAux;
+       
+       return ultimoE;
+    }
+
+    isEmpty(){
+       return this.length() === 0;
+    }
+
+    length(){
+        return this.fila.length();
+    }
+
+    
+    isFull() {
+    return this.length() === this.size;
+    }
+
+    clear(){
+        this.fila.clear();
+    }
+
+    toString() {
+    return this.fila.toString();
+    }
+}
