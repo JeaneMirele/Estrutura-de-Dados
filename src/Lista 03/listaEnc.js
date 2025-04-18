478a159e79d83161efe99f4bc389a285d0e95c6c
JeaneMirele <jeanemirele7@gmail.com>
2025-04-18 12:14:01 -0300

    lista 03

--- src/Lista 03/listaEnc.js
+++ src/Lista 03/listaEnc.js
@@ -3,18 +3,118 @@ import No from "./No";
 class Lista{
     constructor(){
         this.head = new No();
+        this.tamanho = 0;
     }
 
     add(dado){
         let novoNo = new No(dado);
-        novoNo.proximo = head.proximo;
-        head.proximo = novoNo;
+        novoNo.proximo = this.head.proximo;
+        this.head.proximo = novoNo;
     }
 
     append(dado){
-        if(!this.isEmpty()){
-            
+        let novoNo = new No(dado);
+        if(this.isEmpty()){
+            this.head.proximo = novoNo;
+        }else{
+        let atual = this.head;
+        while(atual.proximo !== null){
+            atual = atual.proximo;
         }
-
+        atual.proximo = novoNo;
+        }
     }
+
+    removeFirst(){
+        if(this.isEmpty()){
+           return "Lista Vazia!"
+        }else{
+        let remove = this.head;
+        this.head = this.head.proximo; // faz a cabeça receber o próximo nó, ou seja, ela terá o mesmo valor e ponteiro do nó a que ela apontava;
+        return remove.valor;
+        }
+    }
+
+    removeLast() {
+        if (this.isEmpty()) {
+          return "Lista Vazia!";
+        }
+      
+        let anterior = this.head;
+        let atual = this.head.proximo;
+      
+        if (atual.proximo === null) {
+          this.head.proximo = null;
+          return atual.valor;
+        }
+      
+        while (atual.proximo !== null) {
+          anterior = atual;
+          atual = atual.proximo;
+        }
+      
+        anterior.proximo = null;
+        return atual.valor;
+      }
+
+      removeAt(index) {
+        if (index < 0 || index >= this.length()) return "Índice inválido!";
+      
+        let removido;
+      
+        if (index === 0) {
+          removido = this.head.proximo;
+          this.head.proximo = removido.proximo;
+        } else {
+          let anterior = this.head;
+          for (let i = 0; i < index; i++) {
+            anterior = anterior.proximo;
+          }
+      
+          removido = anterior.proximo;
+          anterior.proximo = removido.proximo;
+        }
+      
+        this.tamanho--;
+        return removido.valor;
+      }
+
+      addAt(index, dado) {
+        if (index < 0 || index > this.length()) return "Índice inválido!";
+      
+        const novoNo = new No(dado);
+      
+        let atual = this.head;
+        for (let i = 0; i < index; i++) {
+          atual = atual.proximo;
+        }
+      
+        novoNo.proximo = atual.proximo;
+        atual.proximo = novoNo;
+      
+        this.tamanho++;
+      }
+      
+
+    search(valorBuscado){ 
+        let atual = this.head;
+        while (atual) {
+          if (atual.valor === valorBuscado) {
+            return true; 
+          }
+          atual = atual.proximo;
+        }
+        return false;
+    }
+
+    isEmpty(){
+        return this.head.proximo === null;
+    }
+
+    length(){
+        return this.tamanho;
+    }   
+          
 }
+
+export default Lista;
